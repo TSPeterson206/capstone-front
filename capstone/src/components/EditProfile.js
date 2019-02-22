2116ae041549a95c56ca988e15a73793e92769b6
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-02-22 11:21:03 -0800

    edit profile functionality works in full

--- capstone/src/components/EditProfile.js
+++ capstone/src/components/EditProfile.js
@@ -14,20 +14,20 @@ export default class EditProfile extends Component {
 
   handleEdit = event => {
     event.preventDefault()
-    if (!this.state.profilepic) {
-      this.setState({
-        profilepic: 'http://res.cloudinary.com/squeaker/image/upload/v1547767064/sbzqwitvw02zyjwzgrld.jpg'
-      })
-    }
+    // if (!this.state.profilepic) {
+    //   this.setState({
+    //     profilepic: 'http://res.cloudinary.com/squeaker/image/upload/v1547767064/sbzqwitvw02zyjwzgrld.jpg'
+    //   })
+    // }
 
-    const { username, password, tagline, profilepic} = event.target
+    const { username, tagline, profilepic, soberdate } = event.target
     console.log(this.props.user)
 
     request(`/users/${this.props.user.id}`, 'put', {
       username:username.value,
-      password:password.value,
       profilepic: profilepic.value,
-      tagline:tagline.value
+      tagline:tagline.value,
+      soberdate:soberdate.value
     })
       .then(response => {
         this.props.history.push({
@@ -41,45 +41,25 @@ export default class EditProfile extends Component {
       })
     }
 
-  onDrop = (acceptedFiles, rejectedFiles) => {
-    acceptedFiles.forEach(file => {
-      const reader = new FileReader()
-      reader.onload = () => {
-        request(`/accounts/${this.props.user.id}/avatar`, 'post', {
-          image: reader.result
-        })
-          .then(result => {
-            this.setState({ profilepic: result.data.image })
+  // onDrop = (acceptedFiles, rejectedFiles) => {
+  //   acceptedFiles.forEach(file => {
+  //     const reader = new FileReader()
+  //     reader.onload = () => {
+  //       request(`/accounts/${this.props.user.id}/avatar`, 'post', {
+  //         image: reader.result
+  //       })
+  //         .then(result => {
+  //           this.setState({ profilepic: result.data.image })
+  //         })
+  //         .catch(error => console.log(error))
+  //     }
+  //     reader.onabort = () => console.log('file reading was aborted')
+  //     reader.onerror = () => console.log('file reading has failed')
 
-          })
-          .catch(error => console.log(error))
-      }
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-
-      reader.readAsDataURL(file)
-    })
-  }
-
-  onDrop = (acceptedFiles, rejectedFiles) => {
-    acceptedFiles.forEach(file => {
-      const reader = new FileReader()
-      reader.onload = () => {
-        request(`/accounts/${this.props.user.id}/avatar`, 'post', {
-          image: reader.result
-        })
-          .then(result => {
-            this.setState({ profilepic: result.data.image })
-          })
-          .catch(error => console.log(error))
-      }
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-
-      reader.readAsDataURL(file)
-    })
-  }
-
+  //     reader.readAsDataURL(file)
+  //   })
+  // }
+ 
   render() {
     return (
       <div className="border rounded p-5 col-sm-8 mt-5 mr-auto ml-auto">
@@ -91,9 +71,31 @@ export default class EditProfile extends Component {
         }
         <h2>Edit Your Profile</h2>
         <form onSubmit={this.handleEdit}>
-
           <br />
-          <Dropzone onDrop={this.onDrop} id='profilepic' name='profilepic'>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control" id="username" name="username"
+              placeholder="What do you want your username to be?" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="tagline">Bio</label>
+            <input
+              type="text" rows="2"
+              className="form-control" id="tagline" name="tagline"
+              placeholder="What do you want your bio to say?" >
+            </input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="profilepic">Profile Picture</label>
+            <input
+              type="text" rows="2"
+              className="form-control" id="profilepic" name="profilepic"
+              placeholder="What do you want your profile picture to be?" >
+            </input>
+          </div>
+          {/* <Dropzone onDrop={this.onDrop} id='profilepic' name='profilepic'>
             {({ getRootProps, getInputProps, isDragActive }) => {
               return (
                 <div
@@ -110,31 +112,14 @@ export default class EditProfile extends Component {
                 </div>
               )
             }}
-          </Dropzone>
-
+          </Dropzone> */}
           <div className="form-group">
-            <label htmlFor="username">Username</label>
+            <label htmlFor="bio">Sobriety Date</label>
             <input
-              type="text"
-              className="form-control" id="username" name="username"
-              placeholder="What do you want your username to be?" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="age">Password</label>
-            <input
-              type="number"
-              className="form-control" id="age" name="age"
-              placeholder="What do you want your new password to be?" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="bio">Bio</label>
-            <textarea
-              type="text" rows="2"
-              className="form-control" id="bio" name="bio"
-              placeholder="What do you want your bio to say?" >
-            </textarea>
+              type="date" rows="2"
+              className="form-control" id="soberdate" name="soberdate"
+              placeholder="What is your sobriety date?" >
+            </input>
           </div>
 
           <button type="submit" className="btn btn-outline-info mr-2">Submit</button>
@@ -147,3 +132,21 @@ export default class EditProfile extends Component {
 }
 
 
+ // onDrop = (acceptedFiles, rejectedFiles) => {
+  //   acceptedFiles.forEach(file => {
+  //     const reader = new FileReader()
+  //     reader.onload = () => {
+  //       request(`/accounts/${this.props.user.id}/avatar`, 'post', {
+  //         image: reader.result
+  //       })
+  //         .then(result => {
+  //           this.setState({ profilepic: result.data.image })
+  //         })
+  //         .catch(error => console.log(error))
+  //     }
+  //     reader.onabort = () => console.log('file reading was aborted')
+  //     reader.onerror = () => console.log('file reading has failed')
+
+  //     reader.readAsDataURL(file)
+  //   })
+  // }
