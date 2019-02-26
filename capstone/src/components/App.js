8d80c5008da9cbb2af9150e8dd15c44ed2c63fcf
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-02-25 22:17:42 -0800

    modded footer to add logo

--- capstone/src/components/App.js
+++ capstone/src/components/App.js
@@ -58,7 +58,7 @@ export default class App extends Component {
                   </Switch>
               </div>
           </BrowserRouter>
-          <Footer className="footer text-center mt-5" bgColor={'grey'} height={75} text={"NextSteps"}>HI</Footer>
+          <Footer className="footer text-center mt-5" bgColor={'lightblue'} height={75}text={<FaShoePrints />}></Footer>
         </div>
       )
     }
